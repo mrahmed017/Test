daed6cc315f3734d8198e22a87ce7a7072ec84d9
Mr Ahmed <71335484+mrahmed017@users.noreply.github.com>
2021-03-17 13:56:28 +0300

    add Var

--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+var names = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
